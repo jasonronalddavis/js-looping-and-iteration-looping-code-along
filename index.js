387f3c5de10fafc337c9b2ef2e4b902a59e802f0
jasonronalddavis <jdavis@c.ringling.edu>
2021-07-27 02:10:18 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+
+
+function writeCards( name, event ) {
+    let cards = []
+    for ( let i = 0; i < name.length; i++ ) {
+      cards.push( `Thank you, ${name[i]}, for the wonderful ${event} gift!` )
+    }
+    return cards
+  }
+
+  function countDown( countdown ) {
+    while ( countdown > 0 ) {
+      console.log( countdown );
+      countdown -= 1;
+    }
+    console.log( countdown );
+  }
